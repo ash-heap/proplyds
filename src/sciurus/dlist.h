0cc35252416dadb34a6662e39e17b87566e51498
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2014-11-19 12:49:49 -0700

    Add doubly linked list.

---
+++ src/sciurus/dlist.h
@@ -0,0 +1,82 @@
+#ifndef SCIURUS_DLIST_H
+#define SCIURUS_DLIST_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+struct DList;
+
+struct DListNode;
+
+
+
+
+struct DList* dlistNew();
+
+struct DList* const dlistCopy(struct DList* const dlist);
+
+void dlistDelete(struct DList* const dlist);
+
+
+
+
+struct DListNode* const dlistHead(struct DList* const dlist);
+
+struct DListNode* const dlistTail(struct DList* const dlist);
+
+struct DListNode* const dlistNodePrev(struct DListNode* node);
+
+struct DListNode* const dlistNodeNext(struct DListNode* node);
+
+
+
+
+void dlistCons(struct DList* const dlist, void* data);
+
+void dlistCat(struct DList* const dlist0, struct DList* const dlist1);
+
+
+
+
+void dlistInsertAtHead(struct DList* const dlist, void* data);
+
+void dlistInsertAtTail(struct DList* const dlist, void* data);
+
+void dlistInsertAfter(struct DList* const dlist,
+                      struct DListNode* const node,
+                      void* data);
+
+void dlistInsertBefore(struct DList* const dlist,
+                       struct DListNode* const node,
+                       void* data);
+
+void dlistDeleteNode(struct DList* const dlist, struct DListNode* const node);
+
+
+
+
+struct DListNode* dlistNodeNew(struct DListNode* const prev,
+                               struct DListNode* const next,
+                               void* data);
+
+void dlistNodeDelete(struct DListNode* const node);
+
+
+
+
+void* dlistNodeData(struct DListNode* const node);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
